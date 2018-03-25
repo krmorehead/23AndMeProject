c551bff3cfe103d708adaf80431c2d6e0b53b5a9
Kyle Morehead <krmorehead@gmail.com>
2018-03-24 18:17:13 -0700

    Added a server. Added a package.json and a bower.json. Added .gitignore

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var session = require('express-session')
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 8001;
+
+var app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+
+console.log(`${__dirname}/../client`)
+app.use(express.static(__dirname+"/../client"));
+
+console.log(`App listening on port ${PORT}`)
+app.listen(PORT)
+
+module.exports = app;
